c528ba0a96a1cd6d972bfe9f8d1639a59df3ccd4
Ifen Faridian <ifeniprun16@gmail.com>
2021-11-15 14:33:49 +0700

    edit footer

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="footer">
       <div class="container">
         <div class="text-center">
-          Copyright © 2021 Team Project Cost Management (@Ade Handiyanto)
+          Copyright © 2021 Team Project Cost Management System
         </div>
       </div>
     </footer>
